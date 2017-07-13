971f2a6b72e45a8f3d55648b75fefe87223f82f1
0legovich <romq1@mail.ru>
2017-07-13 23:47:45 +0300

    Notepad v.1

---
+++ post.rb
@@ -0,0 +1,36 @@
+class Post
+
+  def self.post_types
+    [Memo, Link, Task]
+  end
+
+  def self.create(type_index)
+    post_types[type_index].new
+  end
+  def initialize
+    @created_at = Time.now
+    @text = nil
+  end
+
+  def read_from_console
+
+  end
+
+  def to_stings
+
+  end
+
+  def save
+    file = File.new(file_path, "w")
+    to_strings.each do |string|
+      file.puts(string)
+    end
+    file.close
+  end
+
+  def file_path
+    current_path = File.dirname(__FILE__)
+    file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
+    current_path + "/" + file_name
+  end
+end
--- task.rb
+++ task.rb
@@ -1,3 +1,4 @@
+require 'date'
 class Task < Post
   def initialize
     super
@@ -6,10 +7,18 @@ class Task < Post
   end
 
   def read_from_console
+    puts "Что надо сделать?"
+    @text = STDIN.gets.chomp
 
+    puts "К какому числу? Укажите дату в формате ДД.ММ.ГГГГ, например 01.01.2017"
+    input = STDIN.gets.chomp
+
+    @due_date = Date.parse(input)
   end
 
   def to_strings
-
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+    deadline = "Крайний срок: #{@due_date}"
+    [deadline, @text, time_string]
   end
 end

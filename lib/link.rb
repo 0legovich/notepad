3e12c6d1db1f059d09af2e507051ba00bdc8d734
Shpak Roman <romq1@mail.ru>
2017-10-08 15:11:53 +0300

    Add files via upload

---
+++ lib/link.rb
@@ -0,0 +1,35 @@
+class Link < Post
+  def initialize
+    super
+
+    @url = ''
+  end
+
+  def read_from_console
+    puts "Введите адрес ссылки:"
+    @url = STDIN.gets.chomp
+
+    puts "Что за ссылка?"
+    @text = STDIN.gets.chomp
+  end
+
+  def to_strings
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+    [@url, @text, time_string]
+  end
+
+  def to_db_hash
+    super.merge(
+    {
+      'text' => @text,
+      'url' => @url
+    }
+      )
+  end
+
+  def load_data(data_hash)
+    super(data_hash) #обязателно дергаем родительский метод
+
+    @url = data_hash['url']
+  end
+end
---
+++ lib/memo.rb
@@ -0,0 +1,34 @@
+class Memo < Post
+  def read_from_console
+    puts "Новая заметка (все что до строчки \"end\" будет записано)"
+
+    @text = []
+    line = nil
+
+    until line == "end" do
+      line = STDIN.gets.chomp
+      @text << line
+    end
+
+    @text.pop
+  end
+
+  def to_strings
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+    @text.unshift(time_string)
+  end
+
+  def to_db_hash
+    super.merge(
+    {
+      'text' => @text.join('\n\r')
+    }
+    )
+  end
+
+  def load_data(data_hash)
+    super(data_hash) #обязателно дергаем родительский метод
+
+    @text = data_hash['text'].split('\n\r')
+  end
+end
